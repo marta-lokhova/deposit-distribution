6e9d42fc3ad3a1122d3d4b522e07bea2b841a614
marta-lokhova <marta@stellar.org>
2023-01-17 17:34:52 -0800

    Checkpoint

---
+++ src/lib.rs
@@ -0,0 +1,111 @@
+//! This contract demonstrates 'timelock' concept and implements a
+//! greatly simplified Claimable Balance (similar to
+//! https://developers.stellar.org/docs/glossary/claimable-balance).
+//! The contract allows to deposit some amount of token and allow another
+//! account(s) claim it before or after provided time point.
+//! For simplicity, the contract only supports invoker-based auth.
+#![no_std]
+
+use soroban_sdk::{contractimpl, contracttype, BytesN, Env, Vec};
+
+mod token {
+    soroban_sdk::contractimport!(file = "../soroban_token_spec.wasm");
+}
+
+use token::{Identifier, Signature};
+
+// TODO: audit get, get_unchecked, need to ensure errors are handled gracefully
+// TODO: add admin
+// TODO: add pricing tiers (can be set by admin)
+#[derive(Clone)]
+#[contracttype]
+pub enum DataKey {
+    Admin,
+    Attendees,
+    Price
+}
+
+pub struct DistributionContract;
+
+fn get_attendees(e: &Env) -> Vec<Identifier> {
+    e.storage().get_unchecked(DataKey::Attendees).unwrap()
+}
+
+fn get_price(e: &Env) -> i128 {
+    e.storage().get_unchecked(DataKey::Price).unwrap()
+}
+
+#[contractimpl]
+impl DistributionContract {
+
+    pub fn initialize(
+        e: Env,
+        price: i128,
+    ) {
+        e.storage().set(DataKey::Price, price);
+    }
+
+    pub fn deposit(
+        env: Env,
+        token: BytesN<32>,
+        attendee: Identifier
+    ) {
+        let price = get_price(&env);
+        // Transfer token to this contract address.
+        transfer_from_account_to_contract(&env, &token, &attendee.into(), &price);
+        // TODO: what to do if transfer fails
+        // TODO: maybe store the list of depositors if needed later
+    }
+    
+    pub fn attend(
+        env: Env,
+        attendee: Identifier
+    ) {
+        // Store actual attendees on chain
+        let mut attendees: Vec<Identifier> = get_attendees(&env);
+        attendees.push_back(attendee);
+        env.storage().set(
+            DataKey::Attendees,
+            attendees
+        )
+    }
+
+    // Distribute the money to everyone
+    pub fn withdraw(
+        env: Env,
+        token_id: BytesN<32>
+    ) {
+        let price = get_price(&env);
+
+        let attendees: Vec<Identifier> = get_attendees(&env);
+        for attendee in attendees {
+            transfer_from_contract_to_account(&env, &token_id, &attendee.unwrap(), &price);
+        }
+    }
+}
+
+fn get_contract_id(e: &Env) -> Identifier {
+    Identifier::Contract(e.get_current_contract())
+}
+
+fn transfer_from_account_to_contract(
+    e: &Env,
+    token_id: &BytesN<32>,
+    from: &Identifier,
+    amount: &i128,
+) {
+    let client = token::Client::new(e, token_id);
+    client.xfer_from(&Signature::Invoker, &0, from, &get_contract_id(e), amount);
+}
+
+fn transfer_from_contract_to_account(
+    e: &Env,
+    token_id: &BytesN<32>,
+    to: &Identifier,
+    amount: &i128,
+) {
+    let client = token::Client::new(e, token_id);
+    client.xfer(&Signature::Invoker, &0, to, amount);
+}
+
+mod test;
---
+++ src/test.rs
@@ -0,0 +1,146 @@
+#![cfg(test)]
+
+use super::*;
+use soroban_sdk::testutils::{Accounts, Ledger, LedgerInfo};
+use soroban_sdk::{vec, AccountId, Env, IntoVal};
+
+soroban_sdk::contractimport!(
+    file = "target/wasm32-unknown-unknown/release/soroban_token_contract.wasm"
+);
+
+type TokenClient = Client;
+
+fn create_token_contract(e: &Env, admin: &AccountId) -> (BytesN<32>, TokenClient) {
+    e.install_contract_wasm(WASM);
+
+    let id = e.register_contract_wasm(None, WASM);
+    let token = TokenClient::new(e, &id);
+    // decimals, name, symbol don't matter in tests
+    token.initialize(
+        &Identifier::Account(admin.clone()),
+        &7u32,
+        &"name".into_val(e),
+        &"symbol".into_val(e),
+    );
+    (id, token)
+}
+
+fn create_distribution_contract(e: &Env) -> DistributionContractClient {
+    let distr = DistributionContractClient::new(e, e.register_contract(None, DistributionContract {}));
+    distr.initialize(&200);
+    distr
+}
+
+struct DistributionTest {
+    env: Env,
+    attendee_users: [AccountId; 3],
+    token: TokenClient,
+    token_id: BytesN<32>,
+    contract: DistributionContractClient,
+    contract_id: Identifier,
+}
+
+impl DistributionTest {
+
+    fn setup() -> Self {
+        let env: Env = Default::default();
+        env.ledger().set(LedgerInfo {
+            timestamp: 12345,
+            protocol_version: 1,
+            sequence_number: 10,
+            network_passphrase: Default::default(),
+            base_reserve: 10,
+        });
+
+        let attendee_users = [
+            env.accounts().generate(),
+            env.accounts().generate(),
+            env.accounts().generate(),
+        ];
+
+        let token_admin = env.accounts().generate();
+
+        let (token_id, token) = create_token_contract(&env, &token_admin);
+        for attendee in attendee_users.clone() {
+            token.with_source_account(&token_admin).mint(
+                &Signature::Invoker,
+                &0,
+                &Identifier::Account(attendee.clone()),
+                &1000,
+            );
+        }
+
+        let contract = create_distribution_contract(&env);
+        let contract_id = Identifier::Contract(contract.contract_id.clone());
+        DistributionTest {
+            env,
+            attendee_users,
+            token,
+            token_id,
+            contract,
+            contract_id,
+        }
+    }
+
+    fn deposit(&self, attendee: &Identifier) {
+        self.call_deposit(
+            &self.token_id, &attendee
+        );
+    }
+
+    fn attend(&self, attendee: &Identifier) {
+        self.call_attend(attendee);
+    }
+
+    fn withdraw(&self) {
+        self.call_withdraw(self.token_id.clone());
+    }
+
+    fn call_deposit(
+        &self,
+        token: &BytesN<32>,
+        attendee: &Identifier,
+    ) {
+        self.contract.deposit(token, attendee);
+    }
+
+    fn account_id_to_identifier(&self, account_id: &AccountId) -> Identifier {
+        Identifier::Account(account_id.clone())
+    }
+
+    fn call_withdraw(
+        &self,
+        token_id: BytesN<32>
+    ) {
+        self.contract.withdraw(&token_id);
+    }
+
+    fn call_attend(
+        &self,
+        attendee: &Identifier
+    ) {
+        self.contract.attend(attendee);
+    }
+}
+
+#[test]
+fn test_deposit_attend_and_claim() {
+    let test = DistributionTest::setup();
+
+    // has balance
+    assert_eq!(
+        test.token
+        .balance(&test.account_id_to_identifier(&test.attendee_users[0])),
+        1000
+    );
+    test.deposit(
+        &test.account_id_to_identifier(&test.attendee_users[0])
+    );
+
+    // TODO: give distr accounts ebough balance
+    // assert_eq!(
+    //     test.token
+    //         .balance(&test.contract_id),
+    //     200
+    // );
+}
